63a274980baaca5ee88032ffe1679d2067b098ac
Laurent Payot <laurent.payot@publiberty.com>
2023-09-06 22:49:47 +0200

    gren plugin import fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
 import { defineConfig } from 'vite'
-import { plugin as grenPlugin } from 'vite-plugin-gren'
+import grenPlugin from 'vite-plugin-gren'
 
 export default defineConfig({
     // clearScreen: false,
     server: {
         port: 5000,
         strictPort: true,
-        // fs: {
-        //     // Allow serving files from one level up to the project root
-        //     allow: ['..'],
-        // },
     },
     root: './src',
     build: {
         outDir: `../dist`, // relative to viteConfig.root directory
-        // assetsDir: '../assets', // relative to viteConfig.build.outDir directory
         emptyOutDir: true,
         target: 'es2022'
     },
